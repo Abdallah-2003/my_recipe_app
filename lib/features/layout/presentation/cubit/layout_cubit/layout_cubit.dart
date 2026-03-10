027c3a867a685db9b87979046b9d4c2f10937739
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-03-10 22:53:20 +0200

    layout cubit done

---
+++ lib/features/layout/presentation/cubit/layout_cubit/layout_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_recipe/features/auth/presentation/views/sign_in_view.dart';
+import 'package:my_recipe/features/auth/presentation/views/sign_up_view.dart';
+import 'package:my_recipe/features/home/presentation/views/home_view.dart';
+
+part 'layout_state.dart';
+
+class LayoutCubit extends Cubit<LayoutState> {
+  LayoutCubit() : super(LayoutInitial());
+
+  int currentIndex = 0;
+
+  List<Widget> views = [
+    HomeView(),
+    SignInView(),
+    SignUpView()
+  ];
+
+  changeCurrentIndex(int index) {
+    currentIndex = index;
+    emit(Layoutchange());
+  }
+
+}
--- lib/features/layout/presentation/views/layout_navbar_view.dart
+++ lib/features/layout/presentation/views/layout_navbar_view.dart
@@ -1,25 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_recipe/core/constant/app_colors.dart';
 import 'package:my_recipe/core/constant/app_icons.dart';
 import 'package:my_recipe/core/constant/app_strings.dart';
 import 'package:my_recipe/core/theme/text_styles.dart';
+import 'package:my_recipe/features/layout/presentation/cubit/layout_cubit/layout_cubit.dart';
 
 class LayoutNavbarView extends StatelessWidget {
   const LayoutNavbarView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: AppColors.primary,
-        unselectedItemColor: AppColors.cb8,
-        selectedLabelStyle: AppTextStyles.styleBold10,
-        unselectedLabelStyle: AppTextStyles.styleBold10,
-        items: [
-          BottomNavigationBarItem(icon: Icon(AppIcons.home), label: AppStrings.home),
-          BottomNavigationBarItem(icon: Icon(AppIcons.favorite), label: AppStrings.favorite),
-          BottomNavigationBarItem(icon: Icon(AppIcons.profile), label: AppStrings.profile),
-        ],
+    return BlocProvider(
+      create: (context) => LayoutCubit(),
+      child: BlocBuilder<LayoutCubit, LayoutState>(
+        builder: (context, state) {
+          int currentIndex = context.read<LayoutCubit>().currentIndex; 
+          return Scaffold(
+            body: context.read<LayoutCubit>().views[currentIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: currentIndex,
+              onTap: (index) {
+                context.read<LayoutCubit>().changeCurrentIndex(index);
+              },
+              selectedItemColor: AppColors.primary,
+              unselectedItemColor: AppColors.cb8,
+              selectedLabelStyle: AppTextStyles.styleBold10,
+              unselectedLabelStyle: AppTextStyles.styleBold10,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(AppIcons.home),
+                  label: AppStrings.home,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(AppIcons.favorite),
+                  label: AppStrings.favorite,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(AppIcons.profile),
+                  label: AppStrings.profile,
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
